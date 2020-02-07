07c57d8c64e313493f387290269426ccc7c0d8ad
Jehy <fate@jehy.ru>
2020-02-07 16:33:27 +0300

    V8 coverage support draft

--- integrationTests/passing.test.js
+++ integrationTests/passing.test.js
@@ -7,3 +7,9 @@ it('Works when it has only passing tests', () => {
 it('Works when it has only passing tests and --coverage', () => {
   return expect(runJest('passing', ['--coverage'])).resolves.toMatchSnapshot();
 });
+
+it('Works when it has only passing tests and --coverage with v8', () => {
+  return expect(
+    runJest('passing', ['--coverage', '--coverageProvider', 'v8']),
+  ).resolves.toMatchSnapshot();
+});
--- src/runMocha.js
+++ src/runMocha.js
@@ -1,13 +1,57 @@
 const Mocha = require('mocha');
+const { CoverageInstrumenter } = require('collect-v8-coverage');
+const { fileURLToPath } = require('url');
+const minimatch = require('minimatch');
 const toTestResult = require('./utils/toTestResult');
 const setupCollectCoverage = require('./utils/setupCollectCoverage');
 const getMochaOptions = require('./utils/getMochaOptions');
 
-const runMocha = ({ config, testPath, globalConfig }) => {
-  return new Promise((resolve, reject) => {
-    const { cliOptions: mochaOptions, coverageOptions } = getMochaOptions(
-      config,
-    );
+function runMocha({ config, testPath, globalConfig }) {
+  let v8CoverageInstrumenter;
+  let v8CoverageResult;
+  async function collectV8Coverage() {
+    v8CoverageInstrumenter = new CoverageInstrumenter();
+
+    await v8CoverageInstrumenter.startInstrumenting();
+  }
+
+  async function stopCollectingV8Coverage() {
+    if (!v8CoverageInstrumenter) {
+      throw new Error('You need to call `collectV8Coverage` first.');
+    }
+    v8CoverageResult = await v8CoverageInstrumenter.stopInstrumenting();
+  }
+  function shouldInstrument(file) {
+    return !(/node_modules/.test(file) || config.coveragePathIgnorePatterns.some(pattern => minimatch(file, pattern)));
+  }
+  function getAllV8CoverageInfoCopy() {
+    if (!v8CoverageResult) {
+      throw new Error('You need to `stopCollectingV8Coverage` first');
+    }
+
+    return v8CoverageResult
+      .filter(res => res.url.startsWith('file://'))
+      .map(res => ({ ...res, url: fileURLToPath(res.url) }))
+      .filter(
+        res =>
+          // TODO: will this work on windows? It might be better if `shouldInstrument` deals with it anyways
+          res.url.startsWith(config.rootDir)
+          && shouldInstrument(res.url)
+        // this._fileTransforms.has(res.url) &&
+        //  shouldInstrument(res.url, this._coverageOptions, this._config),
+      )
+      .map(result => {
+        const transformedFile = result.url; // this._fileTransforms.get(result.url);
+
+        return {
+          codeTransformResult: transformedFile,
+          result,
+        };
+      });
+  }
+
+  return new Promise(async (resolve, reject) => {
+    const {cliOptions: mochaOptions, coverageOptions} = getMochaOptions(config);
     let clearMocks;
 
     class Reporter extends Mocha.reporters.Base {
@@ -30,19 +74,26 @@ const runMocha = ({ config, testPath, globalConfig }) => {
           failures.push(test);
         });
         runner.on('pending', test => pending.push(test));
-        runner.on('end', () => {
+        runner.on('end', async () => {
+          let v8Coverage;
           try {
-            resolve(
-              toTestResult({
-                stats: this.stats,
-                tests,
-                pending,
-                failures,
-                passes,
-                coverage: global.__coverage__,
-                jestTestPath: testPath,
-              }),
-            );
+            if (globalConfig.collectCoverage && globalConfig.coverageProvider === 'v8') {
+              await stopCollectingV8Coverage();
+              v8Coverage = getAllV8CoverageInfoCopy();
+            }
+            const result = toTestResult({
+              stats: this.stats,
+              tests,
+              pending,
+              failures,
+              passes,
+              coverage: global.__coverage__,
+              jestTestPath: testPath,
+            });
+            if (v8Coverage && v8Coverage.length > 0) {
+              result.v8Coverage = v8Coverage;
+            }
+            resolve(result);
           } catch (e) {
             reject(e);
           }
@@ -60,13 +111,19 @@ const runMocha = ({ config, testPath, globalConfig }) => {
       require(mochaOptions.compiler);
     }
 
-    setupCollectCoverage({
-      filename: testPath,
-      rootDir: config.rootDir,
-      collectCoverage: globalConfig.collectCoverage,
-      coveragePathIgnorePatterns: config.coveragePathIgnorePatterns,
-      allowBabelRc: coverageOptions.useBabelRc,
-    });
+    if (globalConfig.collectCoverage) {
+      if (globalConfig.coverageProvider === 'v8') {
+        await collectV8Coverage();
+      } else {
+        setupCollectCoverage({
+          filename: testPath,
+          rootDir: config.rootDir,
+          collectCoverage: globalConfig.collectCoverage,
+          coveragePathIgnorePatterns: config.coveragePathIgnorePatterns,
+          allowBabelRc: coverageOptions.useBabelRc,
+        });
+      }
+    }
 
     if (config.setupFilesAfterEnv) {
       config.setupFilesAfterEnv.forEach(path => {
@@ -82,17 +139,16 @@ const runMocha = ({ config, testPath, globalConfig }) => {
     }
 
     mocha.addFile(testPath);
-
     try {
       if (mochaOptions.ui) {
         mocha.ui(mochaOptions.ui).run();
       } else {
         mocha.run();
       }
-    } catch (e) {
-      reject(e);
+    } catch (err) {
+      reject(err);
     }
   });
-};
+}
 
 module.exports = runMocha;

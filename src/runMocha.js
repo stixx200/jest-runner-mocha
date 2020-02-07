5ff0a4535109f9ba21450ec5150360f22d0c5d1a
Jehy <fate@jehy.ru>
2020-02-07 21:42:32 +0300

    small refactoring

--- src/runMocha.js
+++ src/runMocha.js
@@ -6,7 +6,7 @@ const toTestResult = require('./utils/toTestResult');
 const setupCollectCoverage = require('./utils/setupCollectCoverage');
 const getMochaOptions = require('./utils/getMochaOptions');
 
-function runMocha({ config, testPath, globalConfig }) {
+async function runMocha({ config, testPath, globalConfig }) {
   let v8CoverageInstrumenter;
   let v8CoverageResult;
   async function collectV8Coverage() {
@@ -22,7 +22,12 @@ function runMocha({ config, testPath, globalConfig }) {
     v8CoverageResult = await v8CoverageInstrumenter.stopInstrumenting();
   }
   function shouldInstrument(file) {
-    return !(/node_modules/.test(file) || config.coveragePathIgnorePatterns.some(pattern => minimatch(file, pattern)));
+    return !(
+      /node_modules/.test(file) ||
+      config.coveragePathIgnorePatterns.some(pattern =>
+        minimatch(file, pattern),
+      )
+    );
   }
   function getAllV8CoverageInfoCopy() {
     if (!v8CoverageResult) {
@@ -35,8 +40,7 @@ function runMocha({ config, testPath, globalConfig }) {
       .filter(
         res =>
           // TODO: will this work on windows? It might be better if `shouldInstrument` deals with it anyways
-          res.url.startsWith(config.rootDir)
-          && shouldInstrument(res.url)
+          res.url.startsWith(config.rootDir) && shouldInstrument(res.url),
         // this._fileTransforms.has(res.url) &&
         //  shouldInstrument(res.url, this._coverageOptions, this._config),
       )
@@ -50,34 +54,38 @@ function runMocha({ config, testPath, globalConfig }) {
       });
   }
 
-  return new Promise(async (resolve, reject) => {
-    const {cliOptions: mochaOptions, coverageOptions} = getMochaOptions(config);
-    let clearMocks;
+  //  return new Promise(async (resolve, reject) => {
+  const { cliOptions: mochaOptions, coverageOptions } = getMochaOptions(config);
+  let clearMocks;
 
-    class Reporter extends Mocha.reporters.Base {
-      constructor(runner) {
-        super(runner);
-        const tests = [];
-        const pending = [];
-        const failures = [];
-        const passes = [];
+  class Reporter extends Mocha.reporters.Base {
+    constructor(runner) {
+      super(runner);
+      const tests = [];
+      const pending = [];
+      const failures = [];
+      const passes = [];
 
-        runner.on('suite', () => {
-          if (clearMocks) {
-            clearMocks();
-          }
-        });
-        runner.on('test end', test => tests.push(test));
-        runner.on('pass', test => passes.push(test));
-        runner.on('fail', (test, err) => {
-          test.err = err; // eslint-disable-line no-param-reassign
-          failures.push(test);
-        });
-        runner.on('pending', test => pending.push(test));
+      runner.on('suite', () => {
+        if (clearMocks) {
+          clearMocks();
+        }
+      });
+      runner.on('test end', test => tests.push(test));
+      runner.on('pass', test => passes.push(test));
+      runner.on('fail', (test, err) => {
+        test.err = err; // eslint-disable-line no-param-reassign
+        failures.push(test);
+      });
+      runner.on('pending', test => pending.push(test));
+      runner.externalFinish = new Promise((resolve, reject) => {
         runner.on('end', async () => {
           let v8Coverage;
           try {
-            if (globalConfig.collectCoverage && globalConfig.coverageProvider === 'v8') {
+            if (
+              globalConfig.collectCoverage &&
+              globalConfig.coverageProvider === 'v8'
+            ) {
               await stopCollectingV8Coverage();
               v8Coverage = getAllV8CoverageInfoCopy();
             }
@@ -98,57 +106,55 @@ function runMocha({ config, testPath, globalConfig }) {
             reject(e);
           }
         });
-      }
-    }
-
-    const mocha = new Mocha({
-      reporter: Reporter,
-      timeout: mochaOptions.timeout,
-    });
-
-    if (mochaOptions.compiler) {
-      // eslint-disable-next-line import/no-dynamic-require, global-require
-      require(mochaOptions.compiler);
-    }
-
-    if (globalConfig.collectCoverage) {
-      if (globalConfig.coverageProvider === 'v8') {
-        await collectV8Coverage();
-      } else {
-        setupCollectCoverage({
-          filename: testPath,
-          rootDir: config.rootDir,
-          collectCoverage: globalConfig.collectCoverage,
-          coveragePathIgnorePatterns: config.coveragePathIgnorePatterns,
-          allowBabelRc: coverageOptions.useBabelRc,
-        });
-      }
-    }
-
-    if (config.setupFilesAfterEnv) {
-      config.setupFilesAfterEnv.forEach(path => {
-        // eslint-disable-next-line global-require,import/no-dynamic-require
-        const module = require(path);
-        if (config.clearMocks && module.clearMocks) {
-          clearMocks = module.clearMocks;
-        }
       });
     }
-    if (mochaOptions.file) {
-      mochaOptions.file.forEach(file => mocha.addFile(file));
-    }
+  }
 
-    mocha.addFile(testPath);
-    try {
-      if (mochaOptions.ui) {
-        mocha.ui(mochaOptions.ui).run();
-      } else {
-        mocha.run();
-      }
-    } catch (err) {
-      reject(err);
-    }
+  const mocha = new Mocha({
+    reporter: Reporter,
+    timeout: mochaOptions.timeout,
   });
+
+  if (mochaOptions.compiler) {
+    // eslint-disable-next-line import/no-dynamic-require, global-require
+    require(mochaOptions.compiler);
+  }
+
+  if (globalConfig.collectCoverage) {
+    if (globalConfig.coverageProvider === 'v8') {
+      await collectV8Coverage();
+    } else {
+      setupCollectCoverage({
+        filename: testPath,
+        rootDir: config.rootDir,
+        collectCoverage: globalConfig.collectCoverage,
+        coveragePathIgnorePatterns: config.coveragePathIgnorePatterns,
+        allowBabelRc: coverageOptions.useBabelRc,
+      });
+    }
+  }
+
+  if (config.setupFilesAfterEnv) {
+    config.setupFilesAfterEnv.forEach(path => {
+      // eslint-disable-next-line global-require,import/no-dynamic-require
+      const module = require(path);
+      if (config.clearMocks && module.clearMocks) {
+        clearMocks = module.clearMocks;
+      }
+    });
+  }
+  if (mochaOptions.file) {
+    mochaOptions.file.forEach(file => mocha.addFile(file));
+  }
+
+  mocha.addFile(testPath);
+  let runner;
+  if (mochaOptions.ui) {
+    runner = mocha.ui(mochaOptions.ui).run();
+  } else {
+    runner = mocha.run();
+  }
+  return runner.externalFinish;
 }
 
 module.exports = runMocha;

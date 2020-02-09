ba4f948cebeccd6d542bbba186658288c9b4edac
Jehy <fate@jehy.ru>
2020-02-09 21:54:40 +0300

    dont create coverage files during test

--- integrationTests/runJest.js
+++ integrationTests/runJest.js
@@ -51,6 +51,8 @@ function runJest(project, options = []) {
     '--no-watchman',
     '--no-cache',
     '--no-colors',
+    '--coverageReporters',
+    'text',
     '--projects',
     `${projects}`,
   ].concat(options);
